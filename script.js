36e2530d6f7ba1180fcff95ba1ac67a1277ca011
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 22:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+  let inputTags = document.getElementsByTagName('input');
+  let firstName = inputTags[0].value;
+  let lastName = inputTags[1].value;
+  window.alert(`${firstName} ${lastName}`);
 }
